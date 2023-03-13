c837ccfe6e0aa05ad7ed5dc43db5b7e035f7a1b3
santiagosayshey <chaucted@gmail.com>
2023-03-14 06:41:59 +1030

    removed stuff from main

--- exercise1/marks_merge.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-paste <(cut -d "," -f 1 $1) <(cut -d "," -f 2 $1) <(cut -d "," -f 2 $2 ) | head -1
-
-paste <(tail -n+2 $1 | sort | cut -d "," -f 1) <(tail -n+2 $1 | sort | cut -d "," -f 2) <(tail -n+2 $2 | sort | cut -d "," -f 2)
